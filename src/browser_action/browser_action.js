a5c93b836d14429c3c06eb83a9a2dd6a85d6d8ed
Ian Chen <barkatthetree@gmail.com>
2017-08-09 20:03:31 +0800

    Initial commit

---
+++ src/browser_action/browser_action.js
@@ -0,0 +1,63 @@
+
+var app = new Vue({
+    el: '#mainlist',
+    data: {
+        accounts: [
+            {
+                site: "Google",
+                username: "user1"
+            },
+            {
+                site: "Yahoo",
+                username: "user2"
+            },
+            {
+                site: "Google",
+                username: "user1"
+            },
+            {
+                site: "Yahoo",
+                username: "user2"
+            },{
+                site: "Google",
+                username: "user1"
+            },
+            {
+                site: "Yahoo",
+                username: "user2"
+            },{
+                site: "Google",
+                username: "user1"
+            },
+            {
+                site: "Yahoo",
+                username: "user2"
+            },{
+                site: "Google",
+                username: "user1"
+            },
+            {
+                site: "Yahoo",
+                username: "user2"
+            },{
+                site: "Google",
+                username: "user1"
+            },
+            {
+                site: "Yahoo",
+                username: "user2"
+            },{
+                site: "Google",
+                username: "user1"
+            },
+            {
+                site: "Yahoo",
+                username: "user2"
+            }
+        ]
+    }
+});
+window.mdc.autoInit();
+
+
+console.log(app);

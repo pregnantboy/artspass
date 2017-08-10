bed46074611ba99813ebe390b39e83eaa882fd3c
Pregnantboy <barkatthetree@gmail.com>
2017-08-10 12:32:22 +0800

    UI improvements

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,63 +1,74 @@
 
-var app = new Vue({
-    el: '#mainlist',
+var accounts = [
+    {
+        site: "Google",
+        username: "user1"
+    },
+    {
+        site: "Yahoo",
+        username: "user2"
+    },
+    {
+        site: "Google",
+        username: "user1"
+    },
+    {
+        site: "Yahoo",
+        username: "user2"
+    }, {
+        site: "Google",
+        username: "user1"
+    },
+    {
+        site: "Yahoo",
+        username: "user2"
+    }, {
+        site: "Google",
+        username: "user1"
+    },
+    {
+        site: "Yahoo",
+        username: "user2"
+    }, {
+        site: "Google",
+        username: "user1"
+    },
+    {
+        site: "Yahoo",
+        username: "user2"
+    }, {
+        site: "Google",
+        username: "user1"
+    },
+    {
+        site: "Yahoo",
+        username: "user2"
+    }, {
+        site: "Google",
+        username: "user1"
+    },
+    {
+        site: "Yahoo",
+        username: "user2"
+    }
+];
+
+var unfilterdAccounts = _.cloneDeep(accounts);
+
+var mainlist = new Vue({
+    el: '#app',
     data: {
-        accounts: [
-            {
-                site: "Google",
-                username: "user1"
-            },
-            {
-                site: "Yahoo",
-                username: "user2"
-            },
-            {
-                site: "Google",
-                username: "user1"
-            },
-            {
-                site: "Yahoo",
-                username: "user2"
-            },{
-                site: "Google",
-                username: "user1"
-            },
-            {
-                site: "Yahoo",
-                username: "user2"
-            },{
-                site: "Google",
-                username: "user1"
-            },
-            {
-                site: "Yahoo",
-                username: "user2"
-            },{
-                site: "Google",
-                username: "user1"
-            },
-            {
-                site: "Yahoo",
-                username: "user2"
-            },{
-                site: "Google",
-                username: "user1"
-            },
-            {
-                site: "Yahoo",
-                username: "user2"
-            },{
-                site: "Google",
-                username: "user1"
-            },
-            {
-                site: "Yahoo",
-                username: "user2"
-            }
-        ]
+        accounts: accounts,
+        searchString: ""
+    },
+    computed: {
+        filteredAccounts: function () {
+            var that = this;
+            return this.accounts.filter(function (acc) {
+                return (acc.site.toLowerCase().indexOf(that.searchString.toLowerCase()) !== -1)
+            });
+        }
     }
 });
+
 window.mdc.autoInit();
-
-
-console.log(app);

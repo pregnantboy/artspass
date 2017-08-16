d72460a7000b34e625af61bb98f040d3e5647c65
Ian Chen <barkatthetree@gmail.com>
2017-08-16 10:28:07 +0800

    Firebase init

--- src/bg/background.js
+++ src/bg/background.js
@@ -4,10 +4,30 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
+// Initialize Firebase
 
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+var config = {
+  apiKey: "AIzaSyA4p-JEtAvGo5BFKUilv0nDLKbX7qS4e0E",
+  authDomain: "artspass-dev.firebaseapp.com",
+  databaseURL: "https://artspass-dev.firebaseio.com",
+  projectId: "artspass-dev",
+  storageBucket: "",
+  messagingSenderId: "542991076028"
+};
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+var database = firebase.database();
+var databaseRef = database.ref('/arts/accounts/');
+
+databaseRef.on('child_added', function(data) {
+  // send data to browser_action
+});
+
+databaseRef.on('child_changed', function(data) {
+  // send data to browser_action
+});
+
+databaseRef.on('child_removed', function(data) {
+  // send data to browser_action
+});

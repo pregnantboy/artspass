14999de403f2a02e80137861032f3eb0ce932476
Ian Chen <barkatthetree@gmail.com>
2018-02-17 17:48:31 +0800

    Images cropped

--- src/browser_action/lunch/lunch-vehicle.vue
+++ src/browser_action/lunch/lunch-vehicle.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="vehicle-div" style="height:250px;">
 		
-		<img src="../../../icons/svg/cycle.svg" id="vehicle" style="height:150px; position:absolute; top: 70px; left: calc(50% - 75px);" />
+		<img src="../../../icons/svg/automobile.svg" id="vehicle" style="height:150px; position:absolute; top: 70px; left: calc(50% - 75px);" />
 		<svg id="vehicle-shadow" style="position:absolute; top: 140px;">
 			<ellipse cx="200" cy="52" rx="80" ry="6" style="fill:rgba(0,0,0,0.2)" />
 		</svg>
